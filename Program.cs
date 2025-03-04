bed960378d79ab9107729dd4d7da7aea9a23f90e
e.rose <reconroseeri@gmail.com>
2025-03-03 22:04:50 -0500

    remove comments

--- Program.cs
+++ Program.cs
@@ -4,40 +4,12 @@ using Azure.ResourceManager.ResourceGraph;
 using Azure.ResourceManager.ResourceGraph.Models;
 using System.Text.Json;
 using AzDelta;
-using Azure.ResourceManager.Resources;
 using System.Collections.Immutable;
-
-// Console.WriteLine("Hello, World!");
-
-// var json = """
-// {
-//     "id": "/subscriptions/913575a3-0000-0000-0000-4d69586885ef/resourceGroups/rgName/providers/Microsoft.App/containerApps/appName/providers/Microsoft.Resources/changes/08584614150393775807_0c5941e8-78ca-4d40-92c7-9a373466716c",
-//     "tenantId": "1e13d1f5-0000-0000-0000-d4092c853cbd",
-//     "location": "eastus2",
-//     "resourceGroup": "container-resources",
-//     "subscriptionId": "913575a3-0000-0000-0000-4d69586885ef",
-//     "targetResourceType": "microsoft.app/containerapps",
-//     "changedBy": "email@gmail.com",
-//     "clientType": "Azure Portal",
-//     "timestamp": "2025-02-22T02:47:44.399Z",
-//     "operation": "Microsoft.App/containerApps/write",
-//     "changeType": "Update",
-//     "changes": {
-//         "tags.eee": {
-//             "previousValue": "eee",
-//             "newValue": null
-//         }
-//     }
-// }
-// """;
 
 var options = new JsonSerializerOptions
 {
     PropertyNameCaseInsensitive = true
 };
-// var des = JsonSerializer.Deserialize<ResourceChange>(json, options);
-
-// Console.WriteLine(des?.ChangedBy);
 
 var credential = new DefaultAzureCredential(includeInteractiveCredentials: true);
 
